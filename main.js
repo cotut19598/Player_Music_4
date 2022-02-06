a17a0280f07384f5942ce32a3ad3236f0cbe91b8
cotut19598 <90505756+cotut19598@users.noreply.github.com>
2022-02-06 20:43:32 +0700

    Update main.js

--- main.js
+++ main.js
@@ -105,7 +105,7 @@ const app = {
             name: 'Không Bằng',
             singer: 'Na',
             path: './assign/sings/Không Bằng - Na _ Official Music Video.mp3',
-            image: './assign/img/nevada.jpeg'
+            image: './assign/khong bang.jfif'
         }
     ],
     render: function(){
